3fbb43f36690e058027cd919986ec52058de8676
sukeshni <sukeshni.kantrod@gmail.com>
2015-11-02 14:43:17 +0900

    Fixed your point

--- src/test/scala/fizzbuzztest.scala
+++ src/test/scala/fizzbuzztest.scala
@@ -5,15 +5,15 @@ class TestFizzBuzzSpec extends FlatSpec with Matchers {
 
   "fizzBuzz" should " return corresponding 'FizzBuzz'values for number passed" in {
 
-    val data = new Array[String](101)
     for (i <- 1 to 100) {
+      var data = ""
       i match {
-        case i if i % 15 == 0 => data(i) = "FizzBuzz"
-        case i if i % 5  == 0 => data(i) = "Buzz"
-        case i if i % 3  == 0 => data(i) = "Fizz"
-        case _                => data(i) = i.toString
+        case i if i % 15 == 0 => data = "FizzBuzz"
+        case i if i % 5  == 0 => data = "Buzz"
+        case i if i % 3  == 0 => data = "Fizz"
+        case _                => data = i.toString
       }
-      assert(fizzBuzz(i) === data(i))     
+      assert(fizzBuzz(i) === data)     
     }
   } 
 }
