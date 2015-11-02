e1e57398faf069bf57f2fb375d5684551fcb72e9
sukeshni <sukeshni.kantrod@gmail.com>
2015-11-02 17:13:37 +0900

    Remove var result completely

--- src/main/groovy/FizzBuzz.groovy
+++ src/main/groovy/FizzBuzz.groovy
@@ -3,9 +3,9 @@ def fizzBuzz(n) {
   //ここに素数を一文字づつ数列化した際のn番目の数値を出力するプログラムを実装してください。
   //BEGIN_CHALLENGE
 
-  if (n % 15 == 0) result = "FizzBuzz"
-  else if (n % 5 == 0) result = "Buzz"
-  else if (n % 3 == 0) result = "Fizz"
+  if (n % 15 == 0) "FizzBuzz"
+  else if (n % 5 == 0) "Buzz"
+  else if (n % 3 == 0) "Fizz"
   else result = n.toString()
 
   //END_CHALLENGE
