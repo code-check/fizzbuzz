4abb51adc32583434a07bbaed570aa37615cf54a
sukeshni <sukeshni.kantrod@gmail.com>
2016-03-18 13:38:27 +0900

    Small fixes, add answer.md

--- src/fizzbuzz.cs
+++ src/fizzbuzz.cs
@@ -8,14 +8,16 @@ public class FizzBuzz
         //ここに素数判定プログラムを実装してください。
         //BEGIN_CHALLENGE
 
-        string data;
-        if (n % 15 == 0) data = "FizzBuzz";
-        else if (n % 5 == 0) data = "Buzz";
-        else if (n % 3 == 0) data = "Fizz";
-        else data = n.ToString();
+        // string data;
+        // if (n % 15 == 0) data = "FizzBuzz";
+        // else if (n % 5 == 0) data = "Buzz";
+        // else if (n % 3 == 0) data = "Fizz";
+        // else data = n.ToString();
 
-        return data;
+        // return data;
         //END_CHALLENGE
+        // Plese write your solution here and comment the following line
+        return "fizzbuzz";
     }
 
 }
