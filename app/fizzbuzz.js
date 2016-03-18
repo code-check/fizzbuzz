73375f1680500149598b6c419650c30edf2c8801
sukeshni <sukeshni.kantrod@gmail.com>
2016-03-18 13:46:55 +0900

    Add codecheck.yml and answer.md

--- app/fizzbuzz.js
+++ app/fizzbuzz.js
@@ -1,12 +1,12 @@
 function fizzbuzz(data) {
-	//BEGIN_CHALLENGE
-	var result;
-	if(data % 15 == 0) { result = "FizzBuzz"; }
-	else if(data % 5 == 0) {result = "Buzz"; }
-	else if(data %3 ==0 ) {result = "Fizz"; }
-	else result = data.toString();
-	return result;
-	//END_CHALLENGE
+  //BEGIN_CHALLENGE
+  var result;
+  if(data % 15 == 0) { result = "FizzBuzz"; }
+  else if(data % 5 == 0) {result = "Buzz"; }
+  else if(data %3 ==0 ) {result = "Fizz"; }
+  else result = data.toString();
+  return result;
+  //END_CHALLENGE
 
 }
 
