8478c3e015a7bf3123e6f54d851814047e778788
Sukeshni Kantrod <sukeshni.kantrod@gmail.com>
2016-03-24 19:35:56 +0900

    remove return statement
    and show compilation error on running tests w/o solving

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -3,21 +3,19 @@ public class FizzBuzz {
   public static String fizzBuzz(int n) {
     //BEGIN_CHALLENGE
 
-    // String result;
-    // if (n % 15 == 0) {
-    //   result = "FizzBuzz";
-    // }
-    // else if (n % 5 == 0) {
-    //   result = "Buzz";
-    // }
-    // else if (n % 3 ==0) {
-    //   result = "Fizz";
-    // }
-    // else result = Integer.toString(n);
-    // return result;
+    String result;
+    if (n % 15 == 0) {
+      result = "FizzBuzz";
+    }
+    else if (n % 5 == 0) {
+      result = "Buzz";
+    }
+    else if (n % 3 ==0) {
+      result = "Fizz";
+    }
+    else result = Integer.toString(n);
+    return result;
 
     //END_CHALLENGE
-    // Please comment the following line while solving the challenge
-    return "result";
   }
 }
