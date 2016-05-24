ad8606274717682012472e2c121676159bbffe33
shunjikonishi <swandouble@gmail.com>
2016-05-24 15:53:13 +0900

    Add BIGIN/END_CHALLENGE

--- fizzbuzz.lisp
+++ fizzbuzz.lisp
@@ -1,8 +1,10 @@
 (defun fizzbuzz (n) 
+#| BEGIN_CHALLENGE |#
   (cond 
     ((and (= 0 (mod n 3)) (= 0 (mod n 5))) "FizzBuzz")
     ((= 0 (mod n 5)) "Buzz")
     ((= 0 (mod n 3)) "Fizz")
     (T n)
   )
+#| END_CHALLENGE |#
 )
